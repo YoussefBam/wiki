36e5bdb408c1dc7434042dfd3ef355c012f579c0
YoussefBam <[Youssef.bam@outlook.com]>
2024-07-17 18:48:48 +0100

    Set up entry page: Visiting /wiki/TITLE,renders a page that displays the contents of that encyclopedia entry
    If an entry is requested that does not exist, the user is presented with an error page indicating that their requested page was not found.
    If the entry does exist, the user should be presented with a page that displays the content of the entry. The title of the page should include the name of the entry.

---
+++ wiki/wiki1/encyclopedia/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from markdown2 import markdown
+from . import util
+
+
+def index(request):
+    page_name="index"
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries(),"page_name":page_name
+    })
+
+#generate page 
+def generate(request,title):
+    page_name="content_page"
+    try:
+        #get contents of md file and convert it to html
+        content=markdown(util.get_entry(title))
+    except:
+        #case where title is not found
+        content=None
+    finally:
+        return render(request,"encyclopedia/index.html",{
+            "content":content,"title":title, "page_name":page_name
+        })
+
+
+
+
+
+    
+
