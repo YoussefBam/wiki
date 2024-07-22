8c19f1ac682e8ea5fcb99062e0c9cc47004ed5bd
YoussefBam <168854686+YoussefBam@users.noreply.github.com>
2024-07-22 11:59:18 +0100

    Improve views.py's code design

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -10,7 +10,7 @@ def index(request):
         "entries": util.list_entries(),"page_name":page_name
     })
 
-#generate content page 
+#rendering desired page 
 def generate(request,title):
     page_name="content_page"
     try:
@@ -24,19 +24,15 @@ def generate(request,title):
             "content":content,"title":title, "page_name":page_name
         })
     
-#dirty fix that i'm not happy with 
+
 def search(request):
     title=request.GET.get('title')
-    page_name="searched_page"
-    try:
-        #get contents of md file and convert it to html
-        content=markdown(util.get_entry(title))
-    except:
-        #case where title's content page is not found
-        content=None
-    finally:
+    if title in util.list_entries() :
+        return generate(request, title)
+    else :
+        page_name="search_error"
         return render(request,"encyclopedia/index.html",{
-            "content":content,"title":title, "page_name":page_name, "entries": util.list_entries()
+            "title":title, "entries": util.list_entries(),"page_name":page_name
         })
 
 def view_create(request):
@@ -48,7 +44,7 @@ def create(request):
     #get title of desired page to create
     title=request.POST.get('title')
 
-    #case where the title exists (returning error)
+    #case where the title already exists (returning error)
     if title in title_list :
         error=True 
         return render(request,"encyclopedia/add.html", {"error":error, "title":title
@@ -58,19 +54,13 @@ def create(request):
     else :
         #get content
         content=request.POST.get('content')
-        #save content in markdown file.
+        #save content with its title in a markdown file.
         util.save_entry(title, content)
-        #convert content to html
-        content=markdown(util.get_entry(title))
-        #validate page name so index conditonal will be true and will output the contents.
-        page_name="content_page"
-        return render(request,"encyclopedia/index.html",{
-            "content":content,"title":title, "page_name":page_name
-        })
+        #rendering created page
+        return generate(request,title)
 
     
 def edit(request,title):
-        
         #get contents of md file and convert it to html
         content=util.get_entry(title)
         return render(request,"encyclopedia/edit.html", {"content":content,"title":title
@@ -80,22 +70,15 @@ def edit(request,title):
 def save(request,title):
     content=request.POST.get('content')
     util.save_entry(title, content)
-    #convert content to html
-    content=markdown(util.get_entry(title))
-    #validate page name so index conditonal will be true and will output the contents.
-    page_name="content_page"
-    return render(request,"encyclopedia/index.html",{
-        "content":content,"title":title, "page_name":page_name
-    })
+    #rendering created page
+    return generate(request,title)
+    
     
 def random(request):
     title_list=util.list_entries()
     title = choice(title_list)
-    page_name="content_page"
-    content=markdown(util.get_entry(title))
-    return render(request,"encyclopedia/index.html",{
-        "content":content,"title":title, "page_name":page_name
-    })
+    #rendering random page
+    return generate(request,title)
 
 
 
